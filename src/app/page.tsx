11bc5ec5d0eb6a365c91143c0952e2e91a512810
Abdirahman5863 <abdirahmanabdi5863@gmail.com>
2024-10-21 15:22:02 +0300

    pov1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import ThumbnailCreator from "@/components/Thumbnail-creator";
 
 
+
 export default function Home() {
   return (
     <div className="container mx-auto px-4 py-8 bg-gradient-to-r from-red-600 to-red-800 min-h-screen">
       <h1 className="text-4xl font-bold text-center mb-8 text-white">POV YouTube Thumbnail Creator</h1>
-      <ThumbnailCreator />
+      <ThumbnailCreator/>
     </div>
   );
 }
